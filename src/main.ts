d536d9c8c46928f80d461d3531c6090e1e9e8038
wujieli0207 <wujieli0207@outlook.com>
2022-04-18 20:17:43 +0800

    build: 增加全局路由前缀

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,14 @@ import { APP } from '/@/app.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 全局路由前缀
-  // app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');
   // 全局错误过滤器
   app.useGlobalFilters(new HttpExceptionFilter());
   // 全局注册拦截器
   app.useGlobalInterceptors(new TransoformInterceptor());
   // 全局注册管道
   app.useGlobalPipes(new ValidationPipe());
+
   // 设置 swagger 文档
   const config = new DocumentBuilder()
     .setTitle('俏可工作室后端文档')
@@ -26,6 +27,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document); // docs 为访问上下文
+
   await app.listen(APP.port);
 }
 bootstrap();

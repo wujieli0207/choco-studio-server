ce29e37e59b1b662424ba400f7538e1113447322
wujieli0207 <wujieli0207@outlook.com>
2022-04-19 15:24:24 +0800

    style: 删除不使用的代码

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import envConfig from '../config/env';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppointModule } from './modules/appoint/appoint.module';
-import { Appoint } from './modules/appoint/entities/appoint.entity';
-import { UserModule } from './modules/user/user.module';
-import { User } from './modules/user/entities/user.entity';
 import { DatabaseModule } from '/@/processors/database/database.module';
 import { ArticleModule } from '/@/modules/article/article.module';
-import { TagModule } from '/@/modules/tag/tag.module';
+import { TagModule } from './modules/tag/tag.module';
 import { CategoryModule } from '/@/modules/category/category.module';
 
 @Module({
